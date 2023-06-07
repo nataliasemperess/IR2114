eb5bc6c000bd192e5bf9ba091290a876b93f537e
nataliasemperess <al415537@uji.es>
2023-06-07 20:12:57 +0200

    .

--- teoria/OrdenarVectores.cpp
+++ teoria/OrdenarVectores.cpp
@@ -1,21 +1,88 @@
-#define MAX 5   //cantidad de nums de la primera serie
-#define MAX 2   //cantidad de nums de la segunda serie
+#include <iostream> // Biblioteca para E/S estándar
+#include <vector>
+#include <algorithm>
 
-void swap(int * ptr1, int * ptr2){
+const int MAXTALLA = 20; // Define macrollamada con valor de 20  
+
+class Vector{
+private:
+    int *buffer;
+    int talla;
+    void swap(int &, int &);
+public:
+    Vector(int);
+    ~Vector();
+    void ordenar();
+    void leer();
+    void imprimir() const;
+};
+
+// Las funciones constructora y destructora garantizan la inicialización y eliminación de la memoria dinámica
+
+Vector::Vector(int n=MAXTALLA){ // Constructor 
     
-    int temp = *ptr1;
-    *ptr1 = *ptr2;
-    *ptr2 = temp;
+    if (n >= MAXTALLA){
+        talla = MAXTALLA;
+    }
+    else{
+        talla = n;
+        buffer = new int [talla];
+    }
 }
 
-void ordenar(int v[], int talla){ //Ordena el array v por método burbuja
-    int i, j;
-    for (i = talla -1; i>=1; i--){
-        for (j = 0; j < i; j++){
-            if (v[j] > v[j+º]){
-                swap(&v[j], &v[j+1];
+inline Vector::~Vector(){ // Destructor
+    
+    delete [] buffer;
+}
+
+void Vector::ordenar(){ // Ordena el array
+    
+    for (int i = talla -1; i>=1; i--){
+        for (int j = 0; j < i; j++){
+            if (buffer[j] > buffer[j+1]){
+                swap(buffer[j], buffer[j+1]);
             }
         }
     }
 }
 
+
+inline void Vector::swap(int & x, int & y){ // Intercambia los valores de x e y
+    
+    int temp = x;
+    x = y;
+    y = temp;
+}
+
+
+ 
+void Vector::leer(){ // Leer el buffer
+
+    for (int i = 0; i < talla; i++){
+        std::cout<<"Introduce un número: ";
+        std::cin >> buffer[i];
+    }
+}
+
+void Vector::imprimir() const{ //Imprimir el buffer
+            
+    for (int i = 0; i < talla; i++){
+        std::cout << buffer[i]<<" "<<std::endl;
+    }
+}
+
+
+int main(){
+    
+    Vector v1(5), v2(7);
+
+    v1.leer();
+    v1.ordenar();
+    v1.imprimir();
+    
+    v2.leer();
+    v2.ordenar();
+    v2.imprimir();
+    
+    return 0;
+}
